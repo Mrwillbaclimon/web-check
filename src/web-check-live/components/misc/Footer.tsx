f34239e08f16553382fe5f45242934c65affde9f
Mrwillbaclimon <118884402+Mrwillbaclimon@users.noreply.github.com>
2025-03-04 15:26:03 -0500

    Update Footer.tsx

--- src/web-check-live/components/misc/Footer.tsx
+++ src/web-check-live/components/misc/Footer.tsx
@@ -42,18 +42,18 @@ const ALink = styled.a`
 `;
 
 const Footer = (props: { isFixed?: boolean }): JSX.Element => {
-  const licenseUrl = 'https://github.com/lissy93/web-check/blob/master/LICENSE';
-  const authorUrl = 'https://aliciasykes.com';
-  const githubUrl = 'https://github.com/lissy93/web-check';
+  const licenseUrl = https://IPForce.org';
+  const authorUrl = 'NA';
+  const githubUrl = 'https://IPForce.org';
   return (
   <StyledFooter style={props.isFixed ? {position: 'fixed'} : {}}>
     <span>
-      View source at <ALink href={githubUrl}>github.com/lissy93/web-check</ALink>
+      View source at <ALink href={githubUrl}>https://IPForce.org">IPForce.org</ALink>
     </span>
     <span>
       <Link to="/about">Web-Check</Link> is
       licensed under <ALink href={licenseUrl}>MIT</ALink> -
-      © <ALink href={authorUrl}>Alicia Sykes</ALink> 2023
+      © <ALink href={authorUrl}>NA</ALink> 2023
     </span>
   </StyledFooter>
   );
